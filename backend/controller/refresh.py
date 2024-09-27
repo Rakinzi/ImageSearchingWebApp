72a12d55059888b0b4fef46532439e87f630174e
Tapiwa Motsi <tmotsi10@gmail.com>
2024-09-27 23:38:22 +0200

    intergration

--- backend/controller/refresh.py
+++ backend/controller/refresh.py
@@ -2,3 +2,6 @@ import chromadb
 
 chroma_client = chromadb.PersistentClient('../chroma_db/')
 images = chroma_client.delete_collection(name='face_collection')
+
+# chroma_client = chromadb.PersistentClient('./controller/db/')
+# images = chroma_client.delete_collection(name='image_vectors')
